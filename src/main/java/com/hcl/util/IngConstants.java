76172bac93779ccc4157d9ea996f4b8c1fff8554
manisha <manisha9233@gmail.com>
2019-09-05 13:18:25 +0530

    Login API and Test cases commited by Lakshmi

---
+++ src/main/java/com/hcl/util/IngConstants.java
@@ -0,0 +1,11 @@
+package com.hcl.util;
+
+public class IngConstants {
+
+	private IngConstants() {
+		
+	}
+	
+	public static final String LOGIN_FAILURE = "Please Enter Correct Coustmer Id";
+	public static final String LOGIN_SUCCESS = "Logged in Successfully";
+}
