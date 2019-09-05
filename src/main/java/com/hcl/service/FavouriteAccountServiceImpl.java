42e30bacf538259222e46df01d15fd2f5e210070
= <siva3060@gmail.com>
2019-09-05 15:18:01 +0530

    favourite accounts list by shiva

--- src/main/java/com/hcl/service/FavouriteAccountServiceImpl.java
+++ src/main/java/com/hcl/service/FavouriteAccountServiceImpl.java
@@ -3,17 +3,32 @@ package com.hcl.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.hcl.dto.FavouriteAccountDto;
 import com.hcl.entity.Bank;
 import com.hcl.entity.FavouriteAccount;
+import com.hcl.exception.IngBankException;
 import com.hcl.repository.BankRepository;
 import com.hcl.repository.FavouriteAccountRepository;
 
+/**
+ * @author Shiva
+ * 
+ *         This class contains logic to get different account details
+ * 
+ *
+ */
+
 @Service
 public class FavouriteAccountServiceImpl implements FavouriteAccountService {
+
+	private static final Logger logger = LoggerFactory.getLogger(FavouriteAccountServiceImpl.class);
 
 	@Autowired
 	FavouriteAccountRepository favouriteAccountRepository;
@@ -21,36 +36,56 @@ public class FavouriteAccountServiceImpl implements FavouriteAccountService {
 	@Autowired
 	BankRepository bankRepository;
 
+	/**
+	 * @param customerId
+	 * 
+	 * @return list of FavouriteAccountDto Objects
+	 * 
+	 *         This method contains logic to get favourite account list based on
+	 *         customerId
+	 * 
+	 *
+	 */
+
 	@Override
 	public List<FavouriteAccountDto> getAccountsList(Integer customerId) {
+		logger.info("Inside FavouriteAccountServiceImpl customerId:{}", customerId);
 
 		FavouriteAccountDto favouriteAccountDto = null;
+		String status = "ACTIVE";
 
 		List<FavouriteAccountDto> favouriteAccountDtoList = new ArrayList<>();
 
-		List<FavouriteAccount> favouriteAccountList = favouriteAccountRepository.findByCustomerId(customerId);
+		Pageable pageable = PageRequest.of(0, 3);
+		List<FavouriteAccount> favouriteAccountList = favouriteAccountRepository.findByCustomerIdAndStatus(customerId,
+				status, pageable);
 
-		for (FavouriteAccount favouriteAccount : favouriteAccountList) {
+		if (favouriteAccountList.isEmpty()) {
+			throw new IngBankException("No Favourite Accounts added");
+		} else {
+			for (FavouriteAccount favouriteAccount : favouriteAccountList) {
 
-			favouriteAccountDto = new FavouriteAccountDto();
+				favouriteAccountDto = new FavouriteAccountDto();
 
-			favouriteAccountDto.setIbanNumber(favouriteAccount.getIbanNumber());
-			favouriteAccountDto.setAccountName(favouriteAccount.getAccountName());
-			favouriteAccountDto.setFavouriteAccountId(favouriteAccount.getFavouriteAccountId());
+				favouriteAccountDto.setIbanNumber(favouriteAccount.getIbanNumber());
+				favouriteAccountDto.setAccountName(favouriteAccount.getAccountName());
+				favouriteAccountDto.setFavouriteAccountId(favouriteAccount.getFavouriteAccountId());
 
-			String ibanNo = favouriteAccount.getIbanNumber();
-			String ibanList[] = ibanNo.split(" ");
-			String ibanNumber = ibanList[1];
-			Integer bankCode = Integer.parseInt(ibanNumber);
-			List<Bank> bankList = bankRepository.findByBankCode(bankCode);
-			for (Bank bank : bankList) {
-				favouriteAccountDto.setBankName(bank.getBankName());
-				favouriteAccountDtoList.add(favouriteAccountDto);
+				String ibanNo = favouriteAccount.getIbanNumber();
+				String ibanList[] = ibanNo.split(" ");
+				String ibanNumber = ibanList[1];
+				Integer bankCode = Integer.parseInt(ibanNumber);
+				List<Bank> bankList = bankRepository.findByBankCode(bankCode);
+				for (Bank bank : bankList) {
+					favouriteAccountDto.setBankName(bank.getBankName());
+					favouriteAccountDtoList.add(favouriteAccountDto);
+				}
+
 			}
 
-		}
+			return favouriteAccountDtoList;
 
-		return favouriteAccountDtoList;
+		}
 	}
 
 }
